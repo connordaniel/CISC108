104475ea813dadf9a1f0b16be9a3f8f715ea2f78
connordaniel <theconnordaniel@gmail.com>
2018-11-01 15:45:01 -0400

    added lab 10

---
+++ lab10.py
@@ -0,0 +1,84 @@
+# Lab 10: Welcome to Python
+NAMES = ["Connor Daniel", "Olivia Rorke"]
+
+##### Part 1
+
+## Question 1.1) 2pts
+age = 69
+## Question 1.2) 2pts
+title = "But Beautiful"
+## Question 1.3) 2pts
+price = 15.39
+## Question 1.4) 2pts
+recently_read = True
+## Question 1.5) 2pts
+current_year = 2018 * 0 + 8072 - 2018 / 3
+## Question 1.6) 2pts
+can_drink = 21 >= age
+## Question 1.7) 2pts
+message = title + " is my favorite book."
+##### Part 2
+
+## Question 2.1) 5pts
+def average_three(first, second, third)
+	'''
+	Consumes three integers and produces
+	their average.
+
+	Args:
+		first (int): First number
+		second (int): second number
+		third (int): third number
+	Returns:
+		float: average of the three numbers
+	'''
+	total = first + second + third
+	return total / 3
+assert average_three(1,2,3) = 2.0
+assert average_three(4,5,6) = 5.0
+## Question 2.2) 5pts
+def string_join(str1, str2)
+	'''
+	Consumes two strings and produces
+	them joined with a _ inbetween
+
+	Args:
+		str1 (string): first string
+		str2 (string): second string
+	Returns:
+		String: joined string
+	'''
+	return str1 + "_" + str2
+assert string_join("hello","world") = "hello_world"
+assert string_join("corgi","butts") = "corgi_butts"
+## Question 2.3) 5pts
+def square(num)
+	'''
+	Consumes a number and produces the 
+	same number squared
+
+	Args:
+		num (int): number to be squared
+	Returns:
+		int: the squared number
+	'''
+	return num ** 2
+assert square(2) = 4
+assert square(4) = 16
+## Question 2.4) 5pts
+
+## Question 2.5) 6pts
+
+## Question 2.6) 5pts
+
+## Question 2.7) 5pts
+
+##### Part 3
+
+## Question 3.1) 10pts
+
+## Question 3.2) 10pts
+
+## Question 3.3) 10pts
+
+## Question 3.4) 10pts
